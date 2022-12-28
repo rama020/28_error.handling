6c8db07a856710ef2e64f950e2bf38bc1dfdefe5
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-28 22:13:19 +0530

    Add files via upload

---
+++ error_hanling.py
@@ -0,0 +1,34 @@
+'''
+DATE:28TH DEC 2022
+DAY: WEDNESDAY
+TOPIC: ERROR_HANDLING
+AUTHOR:RAMA BHARGAvi
+'''
+print("pytho")
+print("core python")
+try:
+    print(5/0)
+except:
+    print('you have divided by zero')
+print("advance python")
+print("python analytics") 
+print("python")
+print("core python")
+try:
+    print(10/5)
+except:
+    print('not ok divided by zero')
+print("advance anlytice") 
+print("python analytics")      
+print("python")
+print("core python")
+a=5
+try:
+    print('b')
+except ZeroDivisionError:
+    print('not ok divided by zero')
+print("advance python")
+print("python analytics")
+
+
+
